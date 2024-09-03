1ba616298814692f508737d94bb961318f27ee2d
angrmgmt <angrmgmt@gmail.com>
2024-09-02 23:43:16 -0700

    Created Project and Solution, added a single CS file, connected accounts, first commit back.

---
+++ sb-vids/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class CPHInline
+{
+    public bool Execute()
+    {
+        // your main code goes here
+        // See https://aka.ms/new-console-template for more information
+        // Console.WriteLine("Hello, World!");
+
+        return true;
+    }
+}
